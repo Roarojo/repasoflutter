989d6338be4c73e7ddd3d34b42778ede3b3443be
Roarojo <osvaldo.rojo@zapotlanejo.tecmm.edu.mx>
2025-04-16 11:01:35 -0600

    primera carga, plantilla de inicio

---
+++ repasoflutter/android/app/src/main/kotlin/com/example/repasoflutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.repasoflutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ repasoflutter/lib/widget/Repaso1.dart
@@ -0,0 +1,20 @@
+
+
+import 'package:flutter/material.dart';
+
+class Repaso1 extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return Clase();
+  }
+}
+
+class Clase extends State<Repaso1>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+
+}
